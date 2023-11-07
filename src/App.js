f5b75663f0732811d570846787e7d979046f99de
ipb21.k.birkavs <ipb21.k.birkavs@pjvt.local>
2023-11-07 13:41:11 +0200

    mm

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     return (
         <main className="container-all hidden-scrollbar">
             <SlideComponent
-                title={"Sequence diagram"}
+                title={"Sekvences diagramma"}
                 description={"Roberts, Kārlis, Bruno, Justs"}
                 imgUrl={
                     "https://w0.peakpx.com/wallpaper/87/777/HD-wallpaper-hipster-cat-glasses-thumbnail.jpg"
@@ -16,7 +16,7 @@ export default function App() {
             />
 
             <SlideComponent
-                title={"Title of a slide"}
+                title={"Kas tā ir un vai tā ir struktūras vai uzvedības modeļa daļa?"}
                 description={
                     "Lorem lorem rejahb rgueahriofhajbfh oijahebfjo iuhajvebhjfiuo uhegajvnbfiuou heajvbjfiuo hkajebjhkofiu hkjbaehuoi fhjbaeiuohk fjbvnheajiuohfjb kaoeiuhj fbkiuoeahbj fkoiueahjvbnhfjiuo aeuhjvbhfiuo aehkjb"
                 }
@@ -28,7 +28,7 @@ export default function App() {
             />
 
             <SlideComponent
-                title={"Title of a slide"}
+                title={"Kad to izmanto?"}
                 description={
                     "Lorem lorem rejahb rgueahriofhajbfh oijahebfjo iuhajvebhjfiuo uhegajvnbfiuou heajvbjfiuo hkajebjhkofiu hkjbaehuoi fhjbaeiuohk fjbvnheajiuohfjb kaoeiuhj fbkiuoeahbj fkoiueahjvbnhfjiuo aeuhjvbhfiuo aehkjb"
                 }
@@ -36,6 +36,49 @@ export default function App() {
                 bgCol={"rgb(50, 50, 50)"}
                 textCol={"white"}
             />
+
+            <SlideComponent
+                title={"Kādi UML elementi ir sekvences diagrammā?"}
+                description={
+                    "Lorem lorem rejahb rgueahriofhajbfh oijahebfjo iuhajvebhjfiuo uhegajvnbfiuou heajvbjfiuo hkajebjhkofiu hkjbaehuoi fhjbaeiuohk fjbvnheajiuohfjb kaoeiuhj fbkiuoeahbj fkoiueahjvbnhfjiuo aeuhjvbhfiuo aehkjb"
+                }
+                imgUrl={"https://i.imgur.com/79TgQz9.jpg"}
+                bgCol={"rgb(50, 50, 50)"}
+                textCol={"white"}
+            />
+
+            <SlideComponent
+                title={"Kādi UML elementi ir šajā diagrammā?"}
+                description={
+                    "Lorem lorem rejahb rgueahriofhajbfh oijahebfjo iuhajvebhjfiuo uhegajvnbfiuou heajvbjfiuo hkajebjhkofiu hkjbaehuoi fhjbaeiuohk fjbvnheajiuohfjb kaoeiuhj fbkiuoeahbj fkoiueahjvbnhfjiuo aeuhjvbhfiuo aehkjb"
+                }
+                imgUrl={"https://i.imgur.com/79TgQz9.jpg"}
+                bgCol={"rgb(50, 50, 50)"}
+                textCol={"white"}
+            />
+
+            
+            <SlideComponent
+                title={"Kāds ir piemērs ar skaidrojumu?"}
+                description={
+                    "Lorem lorem rejahb rgueahriofhajbfh oijahebfjo iuhajvebhjfiuo uhegajvnbfiuou heajvbjfiuo hkajebjhkofiu hkjbaehuoi fhjbaeiuohk fjbvnheajiuohfjb kaoeiuhj fbkiuoeahbj fkoiueahjvbnhfjiuo aeuhjvbhfiuo aehkjb"
+                }
+                imgUrl={"https://i.imgur.com/79TgQz9.jpg"}
+                bgCol={"rgb(50, 50, 50)"}
+                textCol={"white"}
+            />
+
+            <SlideComponent
+                title={"Kā šī diagramma izskatās Bloga gadījumā?"}
+                description={
+                    "Lorem lorem rejahb rgueahriofhajbfh oijahebfjo iuhajvebhjfiuo uhegajvnbfiuou heajvbjfiuo hkajebjhkofiu hkjbaehuoi fhjbaeiuohk fjbvnheajiuohfjb kaoeiuhj fbkiuoeahbj fkoiueahjvbnhfjiuo aeuhjvbhfiuo aehkjb"
+                }
+                imgUrl={"https://i.imgur.com/79TgQz9.jpg"}
+                bgCol={"rgb(50, 50, 50)"}
+                textCol={"white"}
+            />
+
+            
         </main>
     );
 }
