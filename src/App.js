99ee46e7b170f771128bc735f67726827244dd5d
Bruno <bruno.banionis.b@gmail.com>
2023-11-07 13:30:18 +0200

    slides

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
 
+import SlideComponent from './components/SlideComponent.js';
+
 export default function App( ) {
   return (
-    <main>
-      <h1>olas</h1>
+    <main className='container-all hidden-scrollbar'>
+      <SlideComponent 
+        title={ 'Title of a slide' }
+        description={ 'Lorem lorem rejahb rgueahriofhajbfh oijahebfjo iuhajvebhjfiuo uhegajvnbfiuou heajvbjfiuo hkajebjhkofiu hkjbaehuoi fhjbaeiuohk fjbvnheajiuohfjb kaoeiuhj fbkiuoeahbj fkoiueahjvbnhfjiuo aeuhjvbhfiuo aehkjb' }
+        imgUrl={ 'https://w0.peakpx.com/wallpaper/87/777/HD-wallpaper-hipster-cat-glasses-thumbnail.jpg' }
+        bgCol={ 'rgb(50, 50, 50)' }
+        textCol={ 'white' }
+      />
+
+      <SlideComponent 
+        title={ 'Title of a slide' }
+        description={ 'Lorem lorem rejahb rgueahriofhajbfh oijahebfjo iuhajvebhjfiuo uhegajvnbfiuou heajvbjfiuo hkajebjhkofiu hkjbaehuoi fhjbaeiuohk fjbvnheajiuohfjb kaoeiuhj fbkiuoeahbj fkoiueahjvbnhfjiuo aeuhjvbhfiuo aehkjb' }
+        imgUrl={ 'https://i1.sndcdn.com/artworks-28nZ9iNGE40eOkTI-alYLyg-t500x500.jpg' }
+        bgCol={ 'rgb(50, 50, 50)' }
+        textCol={ 'white' }
+      />
+
+      <SlideComponent 
+        title={ 'Title of a slide' }
+        description={ 'Lorem lorem rejahb rgueahriofhajbfh oijahebfjo iuhajvebhjfiuo uhegajvnbfiuou heajvbjfiuo hkajebjhkofiu hkjbaehuoi fhjbaeiuohk fjbvnheajiuohfjb kaoeiuhj fbkiuoeahbj fkoiueahjvbnhfjiuo aeuhjvbhfiuo aehkjb' }
+        imgUrl={ 'https://i.imgur.com/79TgQz9.jpg' }
+        bgCol={ 'rgb(50, 50, 50)' }
+        textCol={ 'white' }
+      />
     </main>
   );
 }
---
+++ src/components/SlideComponent.js
@@ -0,0 +1,23 @@
+import './SlideComponent.css';
+
+export default function SlideComponent({ title, description, imgUrl, bgCol, textCol }) {
+    return (
+        <article 
+            className='slide'
+            style={{ 
+                '--bgCol': bgCol,
+                '--textCol': textCol
+            }}
+        >
+            <h1>{ title }</h1>
+            <div className='slide-content'>
+                <div className='slide-description'>
+                    { description }
+                </div>
+                <div className='slide-img-container'>
+                    <img src={ imgUrl } />
+                </div>
+            </div>
+        </article>
+    );
+}
