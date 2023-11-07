ecd074c38070abab317bc8c94a99ddbb592c565b
amixam <robertobrinko@gmail.com>
2023-11-07 13:33:29 +0200

    slide 1

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
-import './App.css';
+import "./App.css";
 
-import SlideComponent from './components/SlideComponent.js';
+import SlideComponent from "./components/SlideComponent.js";
 
-export default function App( ) {
-  return (
-    <main className='container-all hidden-scrollbar'>
-      <SlideComponent 
-        title={ 'Title of a slide' }
-        description={ 'Lorem lorem rejahb rgueahriofhajbfh oijahebfjo iuhajvebhjfiuo uhegajvnbfiuou heajvbjfiuo hkajebjhkofiu hkjbaehuoi fhjbaeiuohk fjbvnheajiuohfjb kaoeiuhj fbkiuoeahbj fkoiueahjvbnhfjiuo aeuhjvbhfiuo aehkjb' }
-        imgUrl={ 'https://w0.peakpx.com/wallpaper/87/777/HD-wallpaper-hipster-cat-glasses-thumbnail.jpg' }
-        bgCol={ 'rgb(50, 50, 50)' }
-        textCol={ 'white' }
-      />
+export default function App() {
+    return (
+        <main className="container-all hidden-scrollbar">
+            <SlideComponent
+                title={"Sequence diagram"}
+                description={"Roberts, Kārlis, Bruno, Justs"}
+                imgUrl={
+                    "https://w0.peakpx.com/wallpaper/87/777/HD-wallpaper-hipster-cat-glasses-thumbnail.jpg"
+                }
+                bgCol={"rgb(50, 50, 50)"}
+                textCol={"white"}
+            />
 
-      <SlideComponent 
-        title={ 'Title of a slide' }
-        description={ 'Lorem lorem rejahb rgueahriofhajbfh oijahebfjo iuhajvebhjfiuo uhegajvnbfiuou heajvbjfiuo hkajebjhkofiu hkjbaehuoi fhjbaeiuohk fjbvnheajiuohfjb kaoeiuhj fbkiuoeahbj fkoiueahjvbnhfjiuo aeuhjvbhfiuo aehkjb' }
-        imgUrl={ 'https://i1.sndcdn.com/artworks-28nZ9iNGE40eOkTI-alYLyg-t500x500.jpg' }
-        bgCol={ 'rgb(50, 50, 50)' }
-        textCol={ 'white' }
-      />
+            <SlideComponent
+                title={"Title of a slide"}
+                description={
+                    "Lorem lorem rejahb rgueahriofhajbfh oijahebfjo iuhajvebhjfiuo uhegajvnbfiuou heajvbjfiuo hkajebjhkofiu hkjbaehuoi fhjbaeiuohk fjbvnheajiuohfjb kaoeiuhj fbkiuoeahbj fkoiueahjvbnhfjiuo aeuhjvbhfiuo aehkjb"
+                }
+                imgUrl={
+                    "https://i1.sndcdn.com/artworks-28nZ9iNGE40eOkTI-alYLyg-t500x500.jpg"
+                }
+                bgCol={"rgb(50, 50, 50)"}
+                textCol={"white"}
+            />
 
-      <SlideComponent 
-        title={ 'Title of a slide' }
-        description={ 'Lorem lorem rejahb rgueahriofhajbfh oijahebfjo iuhajvebhjfiuo uhegajvnbfiuou heajvbjfiuo hkajebjhkofiu hkjbaehuoi fhjbaeiuohk fjbvnheajiuohfjb kaoeiuhj fbkiuoeahbj fkoiueahjvbnhfjiuo aeuhjvbhfiuo aehkjb' }
-        imgUrl={ 'https://i.imgur.com/79TgQz9.jpg' }
-        bgCol={ 'rgb(50, 50, 50)' }
-        textCol={ 'white' }
-      />
-    </main>
-  );
+            <SlideComponent
+                title={"Title of a slide"}
+                description={
+                    "Lorem lorem rejahb rgueahriofhajbfh oijahebfjo iuhajvebhjfiuo uhegajvnbfiuou heajvbjfiuo hkajebjhkofiu hkjbaehuoi fhjbaeiuohk fjbvnheajiuohfjb kaoeiuhj fbkiuoeahbj fkoiueahjvbnhfjiuo aeuhjvbhfiuo aehkjb"
+                }
+                imgUrl={"https://i.imgur.com/79TgQz9.jpg"}
+                bgCol={"rgb(50, 50, 50)"}
+                textCol={"white"}
+            />
+        </main>
+    );
 }
